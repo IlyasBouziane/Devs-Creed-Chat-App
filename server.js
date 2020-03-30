6a49cadca8838e29de90ad2c0d6ac694712b03cf
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-03-30 10:07:08 +0100

    setted socketio in front and back end

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
+// We need to work with http module in order to use websockets
+const socketio = require('socket.io')
 const PORT = 3000 || process.env.PORT
 
 const app = express()
+const server = http.createServer(app)
+const io  = socketio(server)
+io.on('connection',socket => console.log('A new WS connection detected'))
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.listen(PORT,() => console.log(`Server started running on port ${PORT}`)) 
+server.listen(PORT,() => console.log(`Server started running on port ${PORT}`)) 
 
